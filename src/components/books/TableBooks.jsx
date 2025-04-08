473b9c45895cc6d19a0f580017e2317d9492a29c
Joyssi <joyssicruz5@gmail.com>
2025-04-07 22:04:53 -0600

    Mejorando enrutamiento.

--- src/components/books/TableBooks.jsx
+++ src/components/books/TableBooks.jsx
@@ -1,70 +1,74 @@
-import React from "react";
-import { Table, Button, Image } from "react-bootstrap";
-import "bootstrap-icons/font/bootstrap-icons.css";
+    import React from "react";
+    import { Table, Button, Image } from "react-bootstrap";
+    import "bootstrap-icons/font/bootstrap-icons.css";
 
-const TablaLibros = ({ libros, openEditModal, openDeleteModal }) => {
-return (
-    <Table striped bordered hover responsive>
-    <thead>
-        <tr>
-        <th>Imagen</th>
-        <th>Título</th>
-        <th>Área Educativa</th>
-        <th>Dirigido a</th>
-        <th>Edición</th>
-        <th>Categoría</th>
-        <th>Descripción</th>
-        <th>PDF</th>
-        <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        {libros.map((libro) => (
-        <tr key={libro.id}>
-            <td>
-            {libro.imagen && (
-                <Image src={libro.imagen} width="140" height="200" />
-            )}
-            </td>
-            <td>{libro.titulo}</td>
-            <td>{libro.area_edu}</td>
-            <td>{libro.dirigido}</td>
-            <td>{libro.edicion}</td>
-            <td>{libro.categoria}</td>
-            <td>{libro.descripcion}</td>
-            <td>
-            {libro.pdfUrl && (
-                <a
-                href={libro.pdfUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Ver PDF
-                </a>
-            )}
-            </td>
-            <td>
-            <Button
-                variant="outline-warning"
-                size="sm"
-                className="me-2"
-                onClick={() => openEditModal(libro)}
-            >
-                <i className="bi bi-pencil"></i>
-            </Button>
-            <Button
-                variant="outline-danger"
-                size="sm"
-                onClick={() => openDeleteModal(libro)}
-            >
-                <i className="bi bi-trash"></i>
-            </Button>
-            </td>
-        </tr>
-        ))}
-    </tbody>
-    </Table>
-);
-};
+    const TablaLibros = ({ libros, openEditModal, openDeleteModal }) => {
+    return (
+        <div style={{ overflowX: "auto" }}>
+        <Table striped bordered hover responsive>
+            <thead>
+            <tr>
+                <th>Imagen</th>
+                <th>Título</th>
+                <th>Área Educativa</th>
+                <th>Dirigido a</th>
+                <th>Edición</th>
+                <th>Categoría</th>
+                <th>Descripción</th>
+                <th>PDF</th>
+                <th>Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            {libros.map((libro) => (
+                <tr key={libro.id}>
+                <td>
+                    {libro.imagen && (
+                    <Image src={libro.imagen} width="140" height="200" />
+                    )}
+                </td>
+                <td>{libro.titulo}</td>
+                <td>{libro.area_edu}</td>
+                <td>{libro.dirigido}</td>
+                <td>{libro.edicion}</td>
+                <td>{libro.categoria}</td>
+                <td>{libro.descripcion}</td>
+                <td>
+                    {libro.pdfUrl && (
+                    <Button
+                        variant="outline-info"
+                        size="sm"
+                        href={libro.pdfUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Ver PDF
+                    </Button>
+                    )}
+                </td>
+                <td>
+                    <Button
+                    variant="outline-warning"
+                    size="sm"
+                    className="me-2"
+                    onClick={() => openEditModal(libro)}
+                    >
+                    <i className="bi bi-pencil"></i>
+                    </Button>
+                    <Button
+                    variant="outline-danger"
+                    size="sm"
+                    onClick={() => openDeleteModal(libro)}
+                    >
+                    <i className="bi bi-trash"></i>
+                    </Button>
+                </td>
+                </tr>
+            ))}
+            </tbody>
+        </Table>
+        </div>
+    );
+    };
 
-export default TablaLibros;
+    export default TablaLibros;
