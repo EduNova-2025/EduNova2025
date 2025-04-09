02415bae415c776281847ed93bf2479fc2857db6
KennRod <rodriguezjosh2003@gmail.com>
2025-04-09 09:37:49 -0600

    Cambios en el diseño del LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,43 +4,46 @@
 
     const LoginForm = ({ email, password, error, setEmail, setPassword, handleSubmit }) => {
     return (
-        <Row className="w-100 justify-content-center">
+      <Row className="w-100 justify-content-center login-background">
         <Col md={6} lg={5} xl={4}>
-            <Card className="p-4 shadow-lg">
+          <Card className="p-4 shadow-lg borderCard">
             <Card.Body>
-                <h3 className="text-center mb-4">Iniciar Sesión</h3>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit={handleSubmit}>
+              <h3 className="text-center mb-4 title">Iniciar Sesión</h3>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="emailUsuario">
-                    <Form.Label>Correo Electrónico</Form.Label>
-                    <Form.Control
+                  <Form.Label className="title">Correo Electrónico</Form.Label>
+                  <Form.Control
                     type="email"
                     placeholder="Ingresa tu correo"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    />
+                    className="animated-input"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="contraseñaUsuario">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control
+                  <Form.Label className="title" >Contraseña</Form.Label>
+                  <Form.Control
                     type="password"
                     placeholder="Ingresa tu contraseña"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    />
+                    className="animated-input"
+                  />
                 </Form.Group>
 
-                <Button variant="primary" type="submit" className="w-100">
-                    Iniciar Sesión
+                <Button type="submit" className="w-100 gradient-button">
+                  Iniciar Sesión
                 </Button>
-                </Form>
+              </Form>
             </Card.Body>
-            </Card>
+          </Card>
         </Col>
-        </Row>
+      </Row>
+
     );
     };
 
