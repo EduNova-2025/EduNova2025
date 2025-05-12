c2d08f6bd9b36a9b737983ed99c6b9bdafeb3ba0
Joyssi <joyssicruz5@gmail.com>
2025-05-12 09:37:17 -0600

    Opciones del Sidebar según el rol.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,11 +7,15 @@ import {
   FaTags,
   FaRobot,
   FaCog,
-  FaSignOutAlt, FaListAlt,
+  FaSignOutAlt,
+  FaListAlt,
   FaComment,
-  FaFolderOpen, FaBook, FaThList, FaVideo,
+  FaFolderOpen,
+  FaBook,
+  FaThList,
+  FaVideo,
 } from "react-icons/fa";
-import { useAuth } from "../../database/authcontext"; // Importar el contexto de autenticación
+import { useAuth } from "../../database/authcontext";
 import "../../styles/Sidebar.css";
 
 const SidebarItem = ({ icon, label, path, active, onClick, isSubmenu }) => (
@@ -30,7 +34,7 @@ const Sidebar = ({ ocultarHamburguesa }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [openSubmenu, setOpenSubmenu] = useState(null);
   const [isHovered, setIsHovered] = useState(false);
-  const { isLoggedIn, logout, userRole } = useAuth(); // Usamos el rol del usuario
+  const { isLoggedIn, logout, userRole } = useAuth();
 
   const toggleSidebar = () => setIsOpen(!isOpen);
 
@@ -58,54 +62,39 @@ const Sidebar = ({ ocultarHamburguesa }) => {
     }
   };
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  // Definir los items del sidebar según el rol
   const items = [
-    { icon: <FaHome />, label: "Inicio", path: "/inicio" },
+    { icon: <FaHome />, label: "Inicio", path: "/inicio", rolesAllowed: ["Admin", "Docente", "Mined"] },
+
     {
       icon: <FaBoxOpen />, label: "Biblioteca Digital", submenuName: "biblioteca", subItems: [
-        { icon: <FaThList />, label: "Categorías", path: "/categorias", rolesAllowed: ["admin"] }, // Solo para admin
-        { icon: <FaBook />, label: "Gestión", path: "/books", rolesAllowed: ["admin"] }, // Solo para admin
-        { icon: <FaFolderOpen />, label: "Catálogo", path: "/catalogo" }, // Disponible para todos
-      ]
+        { icon: <FaThList />, label: "Categorías", path: "/categorias", rolesAllowed: ["Admin", "Mined"] },
+        { icon: <FaBook />, label: "Gestión", path: "/books", rolesAllowed: ["Admin", "Mined"] },
+        { icon: <FaFolderOpen />, label: "Catálogo", path: "/catalogo", rolesAllowed: ["Admin", "Docente"] },
+      ], rolesAllowed: ["Admin", "Docente", "Mined"]
     },
+
     {
       icon: <FaTags />, label: "Teleclases", submenuName: "teleclase", subItems: [
-        { icon: <FaBook />, label: "Gestión", path: "/teleclasemined" },
-        { icon: <FaListAlt />, label: "Documentos", path: "/teleclase" },
-      ]
+        { icon: <FaBook />, label: "Gestión", path: "/teleclasemined", rolesAllowed: ["Admin", "Mined"] },
+        { icon: <FaListAlt />, label: "Documentos", path: "/teleclase", rolesAllowed: ["Admin", "Docente"] },
+      ], rolesAllowed: ["Admin", "Docente", "Mined"]
     },
-    { icon: <FaRobot />, label: "Master IA", path:"/ia" },
-    { icon: <FaComment />, label: "Foro", path: "/foro" },
-    { icon: <FaVideo />, label: "Conferencias", path: "/conferencia" },
-    { icon: <FaCog />, label: "Ajustes", path: "/configuracion" },
+
+    { icon: <FaRobot />, label: "Master IA", path: "/ia", rolesAllowed: ["Admin", "Docente"] },
+    { icon: <FaComment />, label: "Foro", path: "/foro", rolesAllowed: ["Admin", "Docente", "Mined"] },
+    { icon: <FaVideo />, label: "Conferencias", path: "/conferencia", rolesAllowed: ["Admin", "Docente", "Mined"] },
+    { icon: <FaCog />, label: "Ajustes", path: "/configuracion", rolesAllowed: ["Admin", "Docente", "Mined"] },
   ];
 
   const bottomItems = [
     { icon: <FaSignOutAlt />, label: "Cerrar Sesión", path: "/logout" },
   ];
 
-  // Filtrar los items del sidebar según el rol
   const filteredItems = items.filter(item => {
-    if (userRole === "Admin") {
-      return true; // Admin ve todo
-    }
-    if (userRole === "Docente") {
-      // Solo mostrar elementos relevantes para Docente
-      return item.label === "Inicio" || item.label === "Biblioteca Digital" || item.label === "Teleclases" || item.label === "Master IA" || item.label === "Foro" || item.label === "Conferencias";
-    }
-    if (userRole === "Mined") {
-      // Solo mostrar elementos relevantes para MINED
-      return item.label === "Biblioteca Digital" || item.label === "Teleclases" || item.label === "Master IA";
-    }
-    return false;
+    return !item.rolesAllowed || item.rolesAllowed.includes(userRole);
   });
 
   if (!isLoggedIn) return null;
@@ -139,7 +128,7 @@ const Sidebar = ({ ocultarHamburguesa }) => {
                 {openSubmenu === item.submenuName && (
                   <div className="submenu">
                     {item.subItems
-                      .filter(subItem => subItem.rolesAllowed ? subItem.rolesAllowed.includes(userRole) : true) // Filtrar según el rol
+                      .filter(subItem => !subItem.rolesAllowed || subItem.rolesAllowed.includes(userRole))
                       .map((subItem) => (
                         <SidebarItem
                           key={subItem.label}
